2814a960d5f64ffa3a1be3aad72320c88aa5578a
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-23 23:22:50 +0100

    Get and add id for the quote gen quthor gen and button gen

--- public/index.js
+++ public/index.js
@@ -1,16 +1,5 @@
-import fetch from 'node-fetch'
-
 const apiUrl = 'https://zenquotes.io/api/random'
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+const newQuote = document.getElementById('new-quote')
 
-import http from 'http'
-
-const PORT = 3000
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-})
-
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
